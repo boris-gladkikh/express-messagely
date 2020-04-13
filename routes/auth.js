8c3bfc70c58c98f53cd1df98a8504af00e710d08
gladkill <gladkillmusic@gmail.com>
2020-04-13 15:46:45 -0700

    wrote routes for auth and users

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,36 @@
-/** POST /login - login: {username, password} => {token}
- *
- * Make sure to update their last-login!
- *
- **/
+const express = require('express');
+const route = new express.Router();
+const User = require("../models/user.js");
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../config.js");
+const ExpressError = require("../expressError.js")
+
+
+
+
+
+
+// ** POST /login - login: {username, password} => {token}
+//  *
+//  * Make sure to update their last-login!
+//  *
+//  **/
+
+route.post("/login", async function (req, res, next) {
+  let { username, password } = req.body;
+  if (await User.authenticate(username, password) === true) {
+    let payload = { username };
+    let token = jwt.sign(payload, SECRET_KEY);
+    await User.updateLoginTimestamp(username);
+
+    return res.json({ token });
+  } else {
+    let err = new ExpressError("invalid username/password", 400)
+    return next(err);
+  }
+
+})
+
 
 
 /** POST /register - register user: registers, logs in, and returns token.
@@ -11,3 +39,18 @@
  *
  *  Make sure to update their last-login!
  */
+
+route.post("/register", async function (req, res, next) {
+  try {
+    let { username, password, first_name, last_name, phone } = req.body;
+    User.register({ username, password, first_name, last_name, phone });
+    let payload = { username };
+    let token = jwt.sign(payload, SECRET_KEY);
+    await User.updateLoginTimestamp(username);
+    return res.json({ token });
+  } catch (err) {
+    return next(err);
+  }
+
+})
+
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,38 @@
+const express = require('express');
+const route = new express.Router();
+const User = require("../models/user.js");
+const ExpressError = require("../expressError.js");
+
 /** GET / - get list of users.
  *
  * => {users: [{username, first_name, last_name, phone}, ...]}
  *
  **/
 
+route.get("/", async function (req, res, next) {
+  let results = await User.all();
+
+  return res.json({ users: results })
+
+})
 
 /** GET /:username - get detail of users.
  *
  * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
  *
  **/
+
+route.get("/:username", async function (req, res, next) {
+  try {
+    let username = req.params.username;
+    let result = await User.get(username);
+    return res.json({ user: result });
+
+  } catch (err) {
+    return next(err);
+  }
+
+})
 
 
 /** GET /:username/to - get messages to user
@@ -22,6 +45,16 @@
  *
  **/
 
+route.get("/:username/to", async function (req, res, next) {
+  try {
+    let username = req.params.username;
+    let results = await User.messagesTo(username);
+    return res.json({messages:results})
+  }
+  catch (err) {
+    return next(err);
+  }
+})
 
 /** GET /:username/from - get messages from user
  *
@@ -32,3 +65,15 @@
  *                 to_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
+
+route.get("/:username/from", async function(req, res, next) {
+  try {
+    let username = req.params.username;
+    let results = await User.messagesTo(username);
+    return res.json({messages:results})
+  }
+  catch (err) {
+    return next(err);
+  }
+
+})
